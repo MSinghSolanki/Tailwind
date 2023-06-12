60f8cf06ac6f5a339a4df53bfbfa4ae152b1be9f
MSinghSolanki <112701441+MSinghSolanki@users.noreply.github.com>
2023-06-12 18:50:01 +0530

    new

--- food-client/src/components/favourite.jsx
+++ food-client/src/components/favourite.jsx
@@ -3,14 +3,14 @@ import React, { useState,useEffect } from "react";
 
 export const Favourite= ()=>{
     
-const[orders,setOrders] =useState([])
+const[favss,setFavss] =useState([])
 
 
 const fetchOrders = async () => {
   try {
     const response = await axios.get("http://localhost:2754/favourite/fav");
     const data = response.data;
-    setOrders(data.favourites);
+    setFavss(data.stores);
   } catch (error) {
     console.log(error);
   }
@@ -20,36 +20,36 @@ useEffect(() => {
   fetchOrders();
 }, []);
 
+
 return(
     <div>
 <div>
     <h1 className=" text-5xl text-red-500 text-center font-extrabold">Your Fav List</h1>
 </div>
-<div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
-        {orders.map((order, index) => (
+<div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4" >
+        {favss.map((e, ei) => (
           <div
-            key={index}
-            className="border shadow-lg rounded-lg hover:scale-105 duration-300"
+            key={ei}
+            className="boder shadow-lg rounded-lg hover:scale-105 duration-300"
           >
             <img
-              name="image"
-              src={order.image}
-              alt={order.name}
+              src={e.image}
+              alt={e.name}
               className="w-full h-[200px] object-cover rounded-t-lg"
             />
-        
+             
             <div className="flex justify-between px-2 py-4">
-              <p name="name" className="font-bold">
-                {order.name}
-              </p>
-
-              <span name="price" className="bg-orange-500 text-white p-1 rounded-full">
-                {order.price}
+              <p className="font-bold">{e.name}</p>
+             
+              <span className="bg-orange-500 text-white p-1 rounded-full">
+                {e.price}
               </span>
+              
             </div>
           </div>
         ))}
       </div>
+     
     </div>
     
 
